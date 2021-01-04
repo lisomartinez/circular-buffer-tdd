d92a960ad18c22b74de989a9a94b34a74046b331
Lisandro Martinez <lisandromartinez@gmail.com>
2021-01-04 18:21:21 -0300

    add CircularBuffer knows when is full

--- src/circular_buffer/circular_buffer.py
+++ src/circular_buffer/circular_buffer.py
@@ -5,3 +5,6 @@ class CircularBuffer:
 
     def is_empty(self):
         return True
+
+    def is_full(self):
+        return False
--- test/test_circular_buffer.py
+++ test/test_circular_buffer.py
@@ -6,11 +6,13 @@ from assertpy import assert_that
 class TestCircularBuffer:
 
     @pytest.fixture()
-    def buffer(self):
+    def buffer(self) -> CircularBuffer:
         buffer = CircularBuffer()
         yield buffer
         buffer = None
 
-    def test_is_empty_after_creation(self, buffer):
-        buffer_content = buffer.is_empty()
+    def test_is_empty_after_creation(self, buffer: CircularBuffer):
         assert_that(buffer.is_empty()).is_true()
+
+    def test_is_not_null_after_creation(self, buffer: CircularBuffer):
+        assert_that(buffer.is_full()).is_false()
